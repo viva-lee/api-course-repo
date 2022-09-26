38d019cf18de3583240f437e12e59bde6e505644
viva-lee <phoenix71405@gmail.com>
2022-09-26 21:33:11 +0900

    django study

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from rooms.models import Room
 
 # Create your views here.
+def list_rooms(request):
+    rooms = Room.objects.all()
+    print(rooms)
--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(["GET"])
+def list_rooms(request):
+    return Response()
 
 # Create your views here.
